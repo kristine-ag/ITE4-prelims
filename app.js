967ea8bdcdad6b6b5cfe4d67eda6c03747b0ce86
Eiji-Kei <kristineangelagallawan9@gmail.com>
2024-02-20 17:06:24 +0800

    new file:   .gitignore 	new file:   app.js 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/2048.html 	new file:   pages/about.html 	new file:   pages/error.html 	new file:   pages/home.html 	new file:   pages/portfolio.html 	new file:   pages/something.html

---
+++ app.js
@@ -0,0 +1,41 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+
+    let myurl = './pages/'
+    if(req.url == '/home'){
+        myurl += 'home.html'
+        res.statusCode = 200
+    }
+    else if(req.url == '/about'){
+        myurl += 'about.html'
+        res.statusCode = 200
+    }
+    else if(req.url == '/portfolio'){
+        myurl += 'portfolio.html'
+        res.statusCode = 200
+    }
+    else if(req.url == '/something'){
+        myurl += '2048.html'
+        res.statusCode = 200
+    }
+    else{
+        myurl += 'error.html'
+        res.statusCode = 200
+    }
+
+    fs.readFile(myurl, (err, data) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.write(data)
+            res.end()
+        }
+    })
+})
+
+server.listen(4000, 'localhost', () => {
+    console.log('listen hmmMmm')
+})
